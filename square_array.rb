5a63a6dfd9b9da462d9b4d7288b775d33f8cb070
MInh PHO DUC LE <minh.pdl@gmail.com>
2019-05-31 13:16:35 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,23 @@
+
+# def square_array(numbers)
+#   numbers.each {|x| puts x**2}
+# end 
+# arr=[1,2,3,4, 5, 6, 7, 8, 9]
+#   arr.collect {|x| puts x**2}
+
+
+
+# def square_array(numbers)
+#   numbers.each! {|x| puts x**2}
+  
+# end 
+
 def square_array(array)
-  # your code here
+  new_array = []
+  array.each{|a| new_array.push(a*a)}
+  return new_array
 end
+
+numbers = [1, 2, 3]
+
+square_array(numbers)
